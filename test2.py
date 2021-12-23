c029f5572e5227c38b24be53ef3993a16d3e5d35
abrtx <franco.gaudino@yahoo.com>
2021-12-23 14:07:45 -0300

    Add @classmethod examples

--- test2.py
+++ test2.py
@@ -1,4 +1,7 @@
 class Person:
+
+    raise_amt= 1.04
+
     def __init__(self, voto, first, last):
         self.voto = voto
         self.first = first
@@ -6,9 +9,10 @@ class Person:
 
 
 class Employee(Person):
-    def __init__(self, voto, first, last, antiguedad):
+    def __init__(self, voto, first, last, antiguedad, salary):
         super().__init__(voto, first, last)
         self.antiguedad = antiguedad
+        self.salary = salary
 
     @property
     def email(self):
@@ -32,11 +36,29 @@ class Employee(Person):
         return f'Employee({self.first}, {self.last}, {self.antiguedad} anios de antiguedad)'
 
 
-e = Employee('','','','5')
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        voto, first, last, antiguedad, salary = emp_str.split('-')
+        return cls(voto, first, last, antiguedad, salary)
+
+
+emp_str_1 = "si-Renata-Gaudino-8-70000"
+emp_str_2 = "no-Isabel-Gaudino-10-50000"
+emp_str_3 = "si-Florencia-Gaudino-9-80000"
+    
+new_emp = Employee.from_string(emp_str_1)
+
+e = Employee('','','','5','')
 
 e.first="Flo"
 
 e.fullName = 'Florencia Potilla'
+
+Employee.set_raise_amt(1.05)
 
 print("")
 print(e.first)
@@ -45,3 +67,7 @@ print(e.email)
 print(e.fullName)
 print(f'{e.antiguedad} anios de antiguedad')
 print(e)
+print("###### New work ######")
+print(Employee.raise_amt)
+print(e.raise_amt)
+print(new_emp.salary)
